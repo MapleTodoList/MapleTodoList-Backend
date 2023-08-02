0ff7f759639230f6de10e545bea2843c4b9b7326
JarikiSungnyung <hyuk0496@gmail.com>
2023-08-02 20:44:03 +0900

    add level info

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -8,6 +8,9 @@ export class InfoService {
     const Html = await axios.get(`https://maple.gg/u/${name}`);
     const $ = load(Html.data);
     const data = {
+      level: $(
+        '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.user-summary > ul > li:nth-child(1)',
+      ).text(),
       world: $(
         '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > h3 > img',
       ).attr('alt'),
@@ -30,6 +33,11 @@ export class InfoService {
         '#user-profile > section > div.row.row-normal > div.col-lg-4.pt-1.pt-sm-0.pb-1.pb-sm-0.text-center.mt-2.mt-lg-0 > div > div.col-6.col-md-8.col-lg-6 > img',
       ).attr('src'),
     };
+    data.level = data.level.substring(
+      data.level.indexOf('.') + 1,
+      data.level.indexOf('('),
+    );
+    data.level = `${data.level} Lv`;
     data.lanking = data.lanking.substring(0, data.lanking.indexOf('\n'));
     data.lanking = `${data.lanking}위`;
     return data;
